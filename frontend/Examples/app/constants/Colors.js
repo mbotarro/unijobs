192a885d49d43eb92e2bac5293751f04ca41aaad
jvaaguiar <jv.aaguiar@gmail.com>
2019-05-23 10:49:47 -0300

    add constant definitions with colors examples

---
+++ frontend/Examples/app/constants/Colors.js
@@ -0,0 +1,19 @@
+// BASE COLOR PALETTE
+
+const UNIJOBS_COLORS = {
+    main: '#008B8B',
+    dark: '#50514F',
+    dark_grey: '#65737E',
+    light_grey: '#CBD4C2',
+    light: '#FFFCFF',
+}
+
+const ELEMENTS_COLORS = {
+    background: '#FFFFFF',
+
+    button: main,
+    text: 'black',
+
+    title: dark,
+    subtitle: dark_grey,
+}
