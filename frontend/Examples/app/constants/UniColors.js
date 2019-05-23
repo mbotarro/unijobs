5bc3a7ea16d4211adcd83e3ef9fc780514372084
jvaaguiar <jv.aaguiar@gmail.com>
2019-05-23 10:58:31 -0300

    rename colors.js

--- frontend/Examples/app/constants/UniColors.js
+++ frontend/Examples/app/constants/UniColors.js
@@ -1,11 +1,18 @@
-// BASE COLOR PALETTE
+// Sempre definir cores nesse arquivo e usá-las nos outros componentes do app
+// criar uma nova constante para definir cores relacionadas ao mesmo component/conceito
 
+/* BASE COLOR PALETTE */
 const UNIJOBS_COLORS = {
     main: '#008B8B',
     dark: '#50514F',
     dark_grey: '#65737E',
     light_grey: '#CBD4C2',
     light: '#FFFCFF',
+}
+
+const BASIC_COLORS = {
+    white: 'white',
+    black: 'black'
 }
 
 const ELEMENTS_COLORS = {
