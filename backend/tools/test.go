2e613a6a753ee1aa8a158fbaf984d559ab4138b1
Moises Botarro <moises.bottaro.silva@gmail.com>
2019-05-23 22:58:06 -0300

    add create db, migration and drop db to create a db just for test purposes; add drop_db target for local db

--- backend/tools/test.go
+++ backend/tools/test.go
@@ -9,20 +9,18 @@ import (
 
 // GetTestDB returns a connection to a DB used for test
 func GetTestDB() *sqlx.DB {
-	db, err := sqlx.Connect("postgres", "user=postgres dbname=unijobs sslmode=disable host=localhost port=5432")
+	db, err := sqlx.Connect("postgres", "user=postgres dbname=unijobs_test sslmode=disable host=localhost port=5432")
 	if err != nil {
 		log.Panicf("Can't connect to the db")
 	}
-
-	// TODO: init DB test tables
 
 	return db
 }
 
 // CleanDB delete all rows from all DB tables
 func CleanDB(db *sqlx.DB) {
-	db.Exec("DELETE FROM userdata")
-	db.Exec("DELETE FROM offer")
-	db.Exec("DELETE FROM interest")
-	db.Exec("DELETE FROM category")
+	db.MustExec("DELETE FROM userdata")
+	db.MustExec("DELETE FROM offer")
+	db.MustExec("DELETE FROM interest")
+	db.MustExec("DELETE FROM category")
 }
