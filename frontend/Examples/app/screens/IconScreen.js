dfa755413b6f440e3480d7a9aa28879271d72bd8
jvaaguiar <jv.aaguiar@gmail.com>
2019-05-24 11:44:09 -0300

    add icon and text input screens

---
+++ frontend/Examples/app/screens/IconScreen.js
@@ -0,0 +1,33 @@
+"use strict";
+
+import React from 'react';
+import { StyleSheet, Text, TextInput, View, Image } from 'react-native';
+
+import Button from '../components/Button'
+import UniStyles from '../constants/UniStyles'
+
+export default class IconScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={
+            {alignContent:'center', flexDirection: 'row', alignSelf: 'stretch', height: 50}}>
+            <Image
+                style= {{width:50, height:50, alignSelf: 'center'}}
+                source={require('../assets/icons/facebook.png')}
+            />
+            <Text style={[UniStyles.text, {justifyContent: 'center', alignSelf: 'center'}]}> twitter </Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
