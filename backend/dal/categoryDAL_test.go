177979b2435929ded395b49416b15ce6730b2f16
Osmar Chen <osmarchen@Osmarde-MacBook-Air.local>
2019-05-31 13:34:01 -0300

    start implement get all categories in dal

--- backend/dal/categoryDAL_test.go
+++ backend/dal/categoryDAL_test.go
@@ -1,1 +1,44 @@
 package dal_test
+
+import (
+	"testing"
+
+	"gotest.tools/assert"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/mbotarro/unijobs/backend/models"
+	"github.com/mbotarro/unijobs/backend/dal"
+	"github.com/mbotarro/unijobs/backend/tools"
+)
+
+const (
+	insertCategory = `INSERT INTO category (name, description) VALUES ($1, $2)`
+	getCategory    = `SELECT * FROM category WHERE name = $1`
+)
+
+func createFakeCategory(t *testing.T, db *sqlx.DB, name, description string) *models.Category {
+	c := models.Category{}
+
+	db.MustExec(insertCategory, name, description)
+
+	err := db.Get(&c, getCategory, name)
+	assert.Equal(t, err, nil)
+
+	return &c
+}
+
+
+func getCategoryDAL(db *sqlx.DB) *dal.CategoryDAL {
+	return dal.NewCategoryDAL(db)
+}
+
+func TestGetAllCategories(t *testing.T) {
+	db := tools.GetTestDB()
+	defer tools.CleanDB(db)
+
+	categoryDAL := getCategoryDAL(db)
+
+	categories, err := categoryDAL.GetAllCategories()
+
+	for category
+}
