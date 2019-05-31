ddd3a35427f3ee0721c404d6b7b46781b18f4e02
Es7evam <estevam.arantes@usp.br>
2019-05-31 10:38:31 -0300

    Rename interest to request

--- backend/handlers/requestHandler.go
+++ backend/handlers/requestHandler.go
@@ -10,17 +10,17 @@ import (
 	"github.com/mbotarro/unijobs/backend/models"
 )
 
-func createInterestHandler(w http.ResponseWriter, r *http.Request) {
+func createRequestHandler(w http.ResponseWriter, r *http.Request) {
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		panic(err)
 	}
 
 	log.Println(string(body))
-	var interestdata models.Interest
-	err = json.Unmarshal(body, &interestdata)
+	var requestdata models.Request
+	err = json.Unmarshal(body, &requestdata)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Fprintf(w, "You've requested the interest: %s\n", interestdata.Name)
+	fmt.Fprintf(w, "You've requested the interest: %s\n", requestdata.Name)
 }
