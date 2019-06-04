b59d7b8209e9a186939aff2560dd32e8555a8f6e
osmarchen <osmar.chen98@gmail.com>
2019-06-04 09:21:42 -0300

    implement markdown and fix handler

--- backend/docs/API.md
+++ backend/docs/API.md
@@ -23,3 +23,33 @@
     valid: <bool>
 }
 ```
+
+# Category API
+
+## Get All Categories
+
+### GET
+
+```
+/categories
+```
+#### Expected Response
+```
+[
+  {
+    "ID": 1,
+    "Name": "Aulas.Biológicas",
+    "Description": "Aulas de biológicas"
+  },
+  {
+    "ID": 2,
+    "Name": "Aulas.Exatas",
+    "Description": "Aulas de exatas"
+  },
+  {
+    "ID": 3,
+    "Name": "Aulas.Humanas",
+    "Description": "Aulas de humanas"
+  }
+]
+```
--- backend/handlers/categoryHandler.go
+++ backend/handlers/categoryHandler.go
@@ -22,12 +22,12 @@ func NewCategoryHandler(categoryCtrl *usecases.CategoryController) *CategoryHand
 }
 
 func (handler *CategoryHandler) getAllCategories(w http.ResponseWriter, r *http.Request) {
-	valid, err := handler.categoryController.GetAllCategories()
+	categories, err := handler.categoryController.GetAllCategories()
 	if err != nil {
 		http.Error(w, fmt.Errorf("%s:%s", errors.DBQueryError, err.Error()).Error(), http.StatusInternalServerError)
 		return
 	}
 
-	fmt.Println("CATS", valid)
-	tools.WriteStructOnHTTPResponse(valid[0], w)
+	fmt.Println("CATS", categories)
+	tools.WriteStructOnHTTPResponse(categories, w)
 }
