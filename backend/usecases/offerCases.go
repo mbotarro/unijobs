e7e039fd77695967d6fc38bef3c10668c3a43099
Moises Botarro <moises.bottaro.silva@gmail.com>
2019-06-20 11:21:38 -0300

    fix typo in OfferController

--- backend/usecases/offerCases.go
+++ backend/usecases/offerCases.go
@@ -23,19 +23,19 @@ func NewOfferController(db *sqlx.DB, es *elastic.Client) *OfferController {
 
 // GetLastOffers returns all created offers before a given time
 // It only returns a limited number of offers, indicated by the parameter size
-func (rc *OfferController) GetLastOffers(before time.Time, size int) ([]models.Offer, error) {
-	return rc.offerDAL.GetLastOffers(before, size)
+func (oc *OfferController) GetLastOffers(before time.Time, size int) ([]models.Offer, error) {
+	return oc.offerDAL.GetLastOffers(before, size)
 }
 
 // InsertOffer inserts the given offer into the databases (postgres + ES), calling the DAL package function.
 // It returns error != nil in case some error occured.
-func (rc *OfferController) InsertOffer(offer models.Offer) (string, error) {
-	id, err := rc.offerDAL.InsertOfferInDB(&offer)
+func (oc *OfferController) InsertOffer(offer models.Offer) (string, error) {
+	id, err := oc.offerDAL.InsertOfferInDB(&offer)
 	if err != nil {
 		return "", nil
 	}
 
-	err = rc.offerDAL.InsertOfferInES(offer)
+	err = oc.offerDAL.InsertOfferInES(offer)
 	if err != nil {
 		return "", nil
 	}
@@ -45,9 +45,9 @@ func (rc *OfferController) InsertOffer(offer models.Offer) (string, error) {
 
 // SearchOffers searches for offers based on a query sent by the user. It can be filtered by one or more categories whose
 // ids are passed by parameter
-func (rc *OfferController) SearchOffers(query string, categoryIDs ...int) ([]models.Offer, error) {
+func (oc *OfferController) SearchOffers(query string, categoryIDs ...int) ([]models.Offer, error) {
 	// Search for the requests ids in ES
-	ids, err := rc.offerDAL.SearchInES(query, categoryIDs...)
+	ids, err := oc.offerDAL.SearchInES(query, categoryIDs...)
 	if err != nil {
 		return nil, err
 	}
@@ -55,7 +55,7 @@ func (rc *OfferController) SearchOffers(query string, categoryIDs ...int) ([]mod
 	// Get the complete documents in Postgres iff ES returned some requests
 	reqs := make([]models.Offer, 0, len(ids))
 	if len(ids) > 0 {
-		reqs, err = rc.offerDAL.GetOffersByID(ids)
+		reqs, err = oc.offerDAL.GetOffersByID(ids)
 		if err != nil {
 			return nil, err
 		}
